18b0e576be3b52dcdcc464ea95aff6819edd5961
Tomas <tcarneado@gmail.com>
2015-07-13 16:54:09 +0200

    creditos

---
+++ controllers/creditos_controller.js
@@ -0,0 +1,3 @@
+exports.author = function(req,res) {
+	res.render('creditos/author', { name:'Mr. Bean'} );
+}
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var creditosController = require('../controllers/creditos_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -10,5 +11,6 @@ router.get('/', function(req, res) {
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+router.get('/author', creditosController.author);
 
 module.exports = router;
